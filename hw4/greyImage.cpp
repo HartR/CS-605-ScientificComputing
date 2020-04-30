d35a141c7ab55c84f491bb9287de966ae590fa92
Hart Russell <hortisimo@gmail.com>
2020-04-30 12:43:13 -0700

    maybe

--- hw4/greyImage.cpp
+++ hw4/greyImage.cpp
@@ -1,11 +1,12 @@
 #include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
 
-using namespace cv;
+//using namespace cv;
 //! [includes]
 
 int main()
 {
-    std::string image_path = "/home/605/russell/hw4/sunflower.jpg";
+    String image_path = "/home/605/russell/hw4/sunflower.jpg";
     Mat img = imread(image_path, IMREAD_COLOR);
     return 0;
 }
