7c61f43f05e9a8285ce8429eb17af314dcbf0767
Hart Russell <hartcranerussell@gmail.com>
2020-05-14 21:55:55 -0700

    experimental cuda

---
+++ Final Project/cuda_multiply.cu
@@ -0,0 +1,24 @@
+/* multiply.cu */
+#include <cuda.h>
+#include <cuda_runtime.h>
+ 
+ __global__ void __multiply__ (float* a, float* b)
+ {
+ }
+ 
+ extern "C" void call_me_maybe(int blocksize, size_t a_size, size_t b_size, double* mat_a, double* mat_b)
+{
+     /* ... Load CPU data into GPU buffers  */
+    double* mat_a_device;
+    double* mat_b_device;
+    //need to allocate result matrix
+    cudaMalloc((void**)&mat_a_device, a_size);
+    cudaMalloc((void**)&mat_b_device, b_size);
+    cudaMemcpy(mat_a_device, mat_a, a_size, cudaMemcpyHostToDevice);
+    cudaMemcpy(mat_b_device, mat_b, b_size, cudaMemcpyHostToDevice);
+     __multiply__ <<<blocksize, 256>>> (mat_a_device, mat_b_device);
+     cudaFree(mat_a_device);
+     cudaFree(mat_b_device);
+ 
+     /* ... Transfer data from GPU to CPU */
+}
