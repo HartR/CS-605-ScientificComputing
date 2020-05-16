01716e84d7f7c00e00d1c494ad33d0211df1cdf0
Hart Russell <hortisimo@gmail.com>
2020-05-16 11:42:46 -0700

    default commit message

--- Final/compileAndRun.sh
+++ Final/compileAndRun.sh
@@ -2,7 +2,7 @@
 
 chmod 600 /home/605/russell/.ssh/id_rsa
 git pull
-mpic++ -c matrix_multiply.c -o main1.o
+mpic++ -c matrix_multiply.cpp -o main1.o
 nvcc -arch=sm_30 -c cuda_multiply.cu -o cm1.o
 mpic++ main1.o cm1.o -lcudart -L/usr/local/cuda/lib64 -o final.binary
 if [ -f output.txt ]; then
---
+++ Final/matrix_multiply.cpp
@@ -0,0 +1,138 @@
+#include <stdio.h>
+#include "mpi.h"
+#include "mycuda.h"
+#include <stdlib.h>
+
+
+#define N 2
+#define SIZE N*N
+
+using namespace std;
+
+/* global */
+int a[N][N] = {
+    { 1, 2 },
+    { 3, 4 }
+};
+
+int b[N][N] = {
+    { 5, 6 },
+    { 7, 8 }
+};
+
+int c[N][N];
+
+void PopulateMatrices(double* matrix_a, double* matrix_b)
+{
+    for (int j = 0; j < SIZE; j++)
+    {
+        matrix_a[j] = ((double)rand() / (RAND_MAX));
+        matrix_b[j] = ((double)rand() / (RAND_MAX));
+    }
+}
+
+void PrintMatrix(double* matrix, string message)
+{
+    cout << message << endl;
+    for (int row = 0; row < N; row++)
+    {
+        for (int column = 0; column < N; column++)
+            cout << matrix[N * row + column] << " ";
+        cout << endl;
+    }  
+}
+
+
+int main(int argc, char *argv[])
+{
+        
+    double* matrix_a = new double[SIZE];
+    double* matrix_b = new double[SIZE];
+    PopulateMatrices(matrix_a, matrix_b);
+    PrintMatrix(matrix_a, "matrix a before");
+    PrintMatrix(matrix_b, "matrix b before");
+
+
+    MPI_Status status;
+    int me,p;
+    int i,j,k;
+
+
+/* Start up MPI */
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &me);
+    MPI_Comm_size(MPI_COMM_WORLD, &p);
+    MatrixMultiplyCuda(matrix_a, matrix_b, SIZE, p);
+
+    printf("me=%d, p=%d", me, p);
+
+    /* Data distribution */
+    if (me == 0) // master
+    {
+        /*
+        // assume p = 2
+        for (i=1; i<p; i++)
+        {
+            printf("send to  %d with data from: %d and size:%d \n", i, (i)*N/p, N*N/p);
+            MPI_Send(&a[i * N / p][0], N * N / p, MPI_INT, i, 0, MPI_COMM_WORLD);
+            MPI_Send(b, N * N, MPI_INT, i, 0, MPI_COMM_WORLD);
+        }
+        */
+    }
+    else
+    {
+        /*
+        printf("Recv from %d with data from: %d and size:%d \n", 0, (me)*N/p, N*N/p);
+        MPI_Recv(&a[me * N / p][0], N * N / p, MPI_INT, i, 0, MPI_COMM_WORLD, 0);
+        MPI_Recv(b, N * N, MPI_INT, i, 0, MPI_COMM_WORLD, 0);
+        */
+
+    }
+    /* Computation */
+    for(i=me * N / p; i<(me+1) * N/p; ++i)
+    {
+        for (j=0; j < N; j++)
+        {
+            c[i][j] = 0;
+            for (k=0; k<N; k++)
+            {
+                c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+
+    /* Result gathering */
+    if (me != 0 )
+    {
+        MPI_Send(&c[(me) * N/p][0], N*N/p, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    }
+    else
+    {
+        for (i=1; i<p; i++)
+        {
+            MPI_Recv(&c[i * N/p][0], N * N / p, MPI_INT, i, 0, MPI_COMM_WORLD, 0);
+        }
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+
+    /* print the matrix */
+    if (me == 0)
+    {
+        for (i=0; i<N; i++) {
+            printf("\n\t| ");
+            for (j=0; j<N; j++)
+                printf("%2d ", c[i][j]);
+            printf("|");
+        }
+    }
+
+
+    /* Quit */
+
+    MPI_Finalize();
+    return 0;
+}
+
