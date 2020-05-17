e530218d5d21884dbd897b33c3e24613700968c1
Hart Russell <hortisimo@gmail.com>
2020-05-17 13:09:50 -0700

    default commit message

--- Final/mycuda.h
+++ Final/mycuda.h
@@ -5,10 +5,10 @@
 #define SIZE DIMENSION*DIMENSION
 #define HALF SIZE/2
 
-double* matrix_a;
-double* matrix_b;
-double* matrix_result_1;
-double* matrix_result_2;
+extern double* matrix_a;
+extern double* matrix_b;
+extern double* matrix_result_1;
+extern double* matrix_result_2;
 
 void MatrixMultiplyCuda(int host_id);
 
