fdd7f954f45b670e34a286622184aac86afb2537
Hart Russell <hortisimo@gmail.com>
2020-05-17 20:22:53 -0700

    default commit message

--- Final/mycuda.h
+++ Final/mycuda.h
@@ -1,6 +1,6 @@
 #ifndef HEADERFILE_H
 #define HEADERFILE_H
 
-double* result MatrixMultiplyCuda(double* mat_a, double* mat_b, double* mat_result, int m, int n, int k, int host_id);
+void MatrixMultiplyCuda(double* mat_a, double* mat_b, double* mat_result, int m, int n, int k, int host_id);
 
 #endif
