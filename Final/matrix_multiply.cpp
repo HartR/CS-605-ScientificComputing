a4ff068f4541a16e874b8c1efed3f23338621563
Hart Russell <hortisimo@gmail.com>
2020-05-17 19:18:17 -0700

    default commit message

--- Final/matrix_multiply.cpp
+++ Final/matrix_multiply.cpp
@@ -3,10 +3,6 @@
 #include "mycuda.h"
 #include <stdlib.h>
 
-#define DIMENSION 6
-#define SIZE DIMENSION*DIMENSION
-#define HALF SIZE/2
-
 using namespace std;
 
 double* matrix_a;
@@ -16,7 +12,6 @@ double* matrix_result_2;
 int matrix_a_length;
 int matrix_b_length;
 int matrix_result_length;
-//double* merged_matrix;
 
 void PopulateMatrix(double* matrix, int outer, int inner)
 {
@@ -38,21 +33,12 @@ void PrintMatrix(double* matrix, int outer, int inner, string message)
     } 
 }
 
-void PrintHalf(double* matrix)
-{
-    cout << endl;
-    for (int i = 0; i < HALF; i++)
-        cout << i << ": " << matrix[i] << ", ";
-    cout << endl;
-}
-
 double* MergeMatrices()
 {
-    double* merged_matrix = new double[SIZE];
-    for(int i = 0; i < HALF; i++)
+    double* merged_matrix = new double[matrix_result_length];
+    for(int i = matrix_result_length/2; i < matrix_result_length/2; i++)
     {
-        merged_matrix[i] = matrix_result_1[i];
-        merged_matrix[i+HALF] = matrix_result_2[i];
+        matrix_result_1[i] = matrix_result_2[i];
     }
     return merged_matrix;
 }
@@ -60,20 +46,23 @@ double* MergeMatrices()
 
 int main(int argc, char *argv[])
 {
-    // input dimensions of input matrices: matrix_1_height X matrix_1_width_matrix_2_height matrix (A)
-    // matrix_1_width_matrix_2_height X matrix_2_width matrix (B)    
-    int matrix_1_height, matrix_1_width_matrix_2_height, matrix_2_width;
-    matrix_1_height = atoi(argv[1]);
-    matrix_1_width_matrix_2_height = atoi(argv[2]);
-    matrix_2_width = atoi(argv[3]);
-    matrix_a_length = matrix_1_height*matrix_1_width_matrix_2_height;
-    matrix_b_length = matrix_1_width_matrix_2_height*matrix_2_width;
+    // input dimensions of input matrices: 
+    // Matrix A: matrix_a_height  X  matrix_a_width_matrix_b_height 
+    // Matrix B: matrix_a_width_matrix_b_height  X  matrix_b_width    
+    int matrix_a_height, matrix_a_width_matrix_b_height, matrix_b_width;
+    matrix_a_height = atoi(argv[1]);
+    matrix_a_width_matrix_b_height = atoi(argv[2]);
+    matrix_b_width = atoi(argv[3]);
+    matrix_a_length = matrix_a_height * matrix_a_width_matrix_b_height;
+    matrix_b_length = matrix_a_width_matrix_b_height * matrix_b_width;
+    matrix_result_length = matrix_a_height * matrix_b_width;
     matrix_a = new double[matrix_a_length];
     matrix_b = new double[matrix_b_length];
-    matrix_result_1 = new double[matrix_1_height*matrix_2_width];
-    matrix_result_2 = new double[matrix_1_height*matrix_2_width];
-    PopulateMatrix(matrix_a, matrix_1_height, matrix_1_width_matrix_2_height);
-    PopulateMatrix(matrix_b, matrix_1_width_matrix_2_height, matrix_2_width);
+
+    matrix_result_1 = new double[matrix_result_length];
+    matrix_result_2 = new double[matrix_result_length];
+    PopulateMatrix(matrix_a, matrix_a_height, matrix_a_width_matrix_b_height);
+    PopulateMatrix(matrix_b, matrix_a_width_matrix_b_height, matrix_b_width);
 
 
     //PrintMatrix(matrix_result, "matrix result before");
@@ -92,17 +81,17 @@ int main(int argc, char *argv[])
 
     if (current_node == sender) // master
     {
-        MatrixMultiplyCuda(matrix_a, matrix_b, matrix_result_1, matrix_1_height, matrix_1_width_matrix_2_height, matrix_2_width, current_node);
-        MPI_Recv(matrix_result_2, matrix_1_height*matrix_2_width, MPI_DOUBLE, receiver, tag_unused, MPI_COMM_WORLD, &status);
+        MatrixMultiplyCuda(matrix_a, matrix_b, matrix_result_1, matrix_a_height, matrix_a_width_matrix_b_height, matrix_b_width, current_node);
+        MPI_Recv(matrix_result_2, matrix_a_height*matrix_b_width, MPI_DOUBLE, receiver, tag_unused, MPI_COMM_WORLD, &status);
     }
     else if (current_node == receiver) // master
     {
-        MatrixMultiplyCuda(matrix_a, matrix_b, matrix_result_2, matrix_1_height, matrix_1_width_matrix_2_height, matrix_2_width, current_node);
-        MPI_Send(matrix_result_2, matrix_1_height*matrix_2_width, MPI_DOUBLE, sender, tag_unused, MPI_COMM_WORLD);
+        MatrixMultiplyCuda(matrix_a, matrix_b, matrix_result_2, matrix_a_height, matrix_a_width_matrix_b_height, matrix_b_width, current_node);
+        MPI_Send(matrix_result_2, matrix_a_height*matrix_b_width, MPI_DOUBLE, sender, tag_unused, MPI_COMM_WORLD);
     }
     /*
     MPI_Datatype offset_mpi_vector;
-    \jm hyg6]matrix_1_height  x6PI_Type_vector( 2, 2, 4, MPI_DOUBLE, &offset_mpi_vector);
+    \jm hyg6]matrix_a_height  x6PI_Type_vector( 2, 2, 4, MPI_DOUBLE, &offset_mpi_vector);
     MPI_Type_commit(&offset_mpi_vector);
     */
 
@@ -113,9 +102,11 @@ int main(int argc, char *argv[])
     /* Data distribution */
     if (current_node == sender) // master
     {
-        PrintMatrix(matrix_result_2, matrix_1_height, matrix_2_width, "In sender, printing mat res 2");
+        PrintMatrix(matrix_result_2, matrix_a_height, matrix_b_width, "In sender, printing mat res 2");
 
-        PrintMatrix(matrix_result_1, matrix_1_height, matrix_2_width, "In sender, printing mat res 1");
+        PrintMatrix(matrix_result_1, matrix_a_height, matrix_b_width, "In sender, printing mat res 1");
+
+        PrintMatrix(MergeMatrices(), "merged matrix");
         //PrintMatrix(matrix_result, "before first multiply?");
         //MatrixMultiplyCuda(matrix_a, matrix_b, matrix_result_1, SIZE, current_node);
 
@@ -134,17 +125,17 @@ int main(int argc, char *argv[])
         // assume p = 2
         for (i=1; i<p; i++)
         {
-            printf("send to  %d with data from: %d and size:%d \matrix_1_width_matrix_2_height", i, (i)*matrix_1_width_matrix_2_height/p, matrix_1_width_matrix_2_height*matrix_1_width_matrix_2_height/p);
-            MPI_Send(&a[i * matrix_1_width_matrix_2_height / p][0], matrix_1_width_matrix_2_height * matrix_1_width_matrix_2_height / p, MPI_INT, i, 0, MPI_COMM_WORLD);
-            MPI_Send(b, matrix_1_width_matrix_2_height * matrix_1_width_matrix_2_height, MPI_INT, i, 0, MPI_COMM_WORLD);
+            printf("send to  %d with data from: %d and size:%d \matrix_a_width_matrix_b_height", i, (i)*matrix_a_width_matrix_b_height/p, matrix_a_width_matrix_b_height*matrix_a_width_matrix_b_height/p);
+            MPI_Send(&a[i * matrix_a_width_matrix_b_height / p][0], matrix_a_width_matrix_b_height * matrix_a_width_matrix_b_height / p, MPI_INT, i, 0, MPI_COMM_WORLD);
+            MPI_Send(b, matrix_a_width_matrix_b_height * matrix_a_width_matrix_b_height, MPI_INT, i, 0, MPI_COMM_WORLD);
         }
         */
     }
     else if (current_node = receiver) // second node
     {
-       PrintMatrix(matrix_result_2, matrix_1_height, matrix_2_width, "In receiver, printing mat res 2");
+       PrintMatrix(matrix_result_2, matrix_a_height, matrix_b_width, "In receiver, printing mat res 2");
 
-       PrintMatrix(matrix_result_1, matrix_1_height, matrix_2_width, "In receiver, printing mat res 1");
+       PrintMatrix(matrix_result_1, matrix_a_height, matrix_b_width, "In receiver, printing mat res 1");
 
         /*
         MPI_Recv(matrix_a, SIZE, MPI_DOUBLE, sender, tag_unused, MPI_COMM_WORLD, &status);
@@ -161,12 +152,13 @@ int main(int argc, char *argv[])
 
         //PrintMatrix(matrix_result, "did it work?");
         /*
-        printf("Recv from %d with data from: %d and size:%d \matrix_1_width_matrix_2_height", 0, (me)*matrix_1_width_matrix_2_height/p, matrix_1_width_matrix_2_height*matrix_1_width_matrix_2_height/p);
-        MPI_Recv(&a[me * matrix_1_width_matrix_2_height / p][0], matrix_1_width_matrix_2_height * matrix_1_width_matrix_2_height / p, MPI_INT, i, 0, MPI_COMM_WORLD, 0);
-        MPI_Recv(b, matrix_1_width_matrix_2_height * matrix_1_width_matrix_2_height, MPI_INT, i, 0, MPI_COMM_WORLD, 0);
+        printf("Recv from %d with data from: %d and size:%d \matrix_a_width_matrix_b_height", 0, (me)*matrix_a_width_matrix_b_height/p, matrix_a_width_matrix_b_height*matrix_a_width_matrix_b_height/p);
+        MPI_Recv(&a[me * matrix_a_width_matrix_b_height / p][0], matrix_a_width_matrix_b_height * matrix_a_width_matrix_b_height / p, MPI_INT, i, 0, MPI_COMM_WORLD, 0);
+        MPI_Recv(b, matrix_a_width_matrix_b_height * matrix_a_width_matrix_b_height, MPI_INT, i, 0, MPI_COMM_WORLD, 0);
         */
 
     }
+    
     //send matrix result from node 2 back to original node
     /*MPI_Barrier(MPI_COMM_WORLD);
 
@@ -201,14 +193,14 @@ int main(int argc, char *argv[])
 
     /* Computation */
     /*
-    for(i=me * matrix_1_width_matrix_2_height / p; i<(me+1) * matrix_1_width_matrix_2_height/p; ++i)
+    for(i=me * matrix_a_width_matrix_b_height / p; i<(me+1) * matrix_a_width_matrix_b_height/p; ++i)
     {
-        for (j=0; j < matrix_1_width_matrix_2_height; j++)
+        for (j=0; j < matrix_a_width_matrix_b_height; j++)
         {
             c[i][j] = 0;
-            for (matrix_2_width=0; matrix_2_width<matrix_1_width_matrix_2_height; matrix_2_width++)
+            for (matrix_b_width=0; matrix_b_width<matrix_a_width_matrix_b_height; matrix_b_width++)
             {
-                c[i][j] += a[i][matrix_2_width] * b[matrix_2_width][j];
+                c[i][j] += a[i][matrix_b_width] * b[matrix_b_width][j];
             }
         }
     }
@@ -216,13 +208,13 @@ int main(int argc, char *argv[])
     // Result gathering 
     if (me != 0 )
     {
-        MPI_Send(&c[(me) * matrix_1_width_matrix_2_height/p][0], matrix_1_width_matrix_2_height*matrix_1_width_matrix_2_height/p, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        MPI_Send(&c[(me) * matrix_a_width_matrix_b_height/p][0], matrix_a_width_matrix_b_height*matrix_a_width_matrix_b_height/p, MPI_INT, 0, 0, MPI_COMM_WORLD);
     }
     else
     {
         for (i=1; i<p; i++)
         {
-            MPI_Recv(&c[i * matrix_1_width_matrix_2_height/p][0], matrix_1_width_matrix_2_height * matrix_1_width_matrix_2_height / p, MPI_INT, i, 0, MPI_COMM_WORLD, 0);
+            MPI_Recv(&c[i * matrix_a_width_matrix_b_height/p][0], matrix_a_width_matrix_b_height * matrix_a_width_matrix_b_height / p, MPI_INT, i, 0, MPI_COMM_WORLD, 0);
         }
     }
 
@@ -232,9 +224,9 @@ int main(int argc, char *argv[])
     // print the matrix 
     if (me == 0)
     {
-        for (i=0; i<matrix_1_width_matrix_2_height; i++) {
-            printf("\matrix_1_width_matrix_2_height\t| ");
-            for (j=0; j<matrix_1_width_matrix_2_height; j++)
+        for (i=0; i<matrix_a_width_matrix_b_height; i++) {
+            printf("\matrix_a_width_matrix_b_height\t| ");
+            for (j=0; j<matrix_a_width_matrix_b_height; j++)
                 printf("%2d ", c[i][j]);
             printf("|");
         }
