682800e04b34ccc782829fa3811f37a86e9ec0f5
Hart Russell <hortisimo@gmail.com>
2020-05-17 18:44:03 -0700

    probably broke it

--- Final/cuda_multiply.cu
+++ Final/cuda_multiply.cu
@@ -17,12 +17,12 @@ using namespace std;
      int i = blockIdx.y * blockDim.y + threadIdx.y; 
      int j = blockIdx.x * blockDim.x + threadIdx.x;
      
-     if( j < matrix_2_width && i < matrix_1_height) 
+     if( j < matrix_2_width/2 && i < matrix_1_height/2) 
      {
           
          for(int k = 0; k < matrix_1_width_matrix_2_height; k++) 
          {
-               c[i * matrix_2_width + j] += a[i * matrix_1_width_matrix_2_height + matrix_2_width] * b[matrix_2_width * matrix_2_width + j];
+               c[i * matrix_2_width + j + offset] += a[i * matrix_1_width_matrix_2_height + matrix_2_width + offset] * b[matrix_2_width * matrix_2_width + j + offset];
                //printf("\ni is %d, a is %f, b is %f", i, a[i * matrix_1_width_matrix_2_height + i], b[i * matrix_2_width + j]);
          }
          //printf("\matrix_1_width_matrix_2_height At location %d, in c, assigned value %f, sum is %f, value of a is %f, val of b is %f", i * matrix_2_width + j + offset, c[i * matrix_2_width + j + offset], a[i], b[i]);    
